0cf9a6108c3bc9479c78d8561359dcb8d8d452fa
Aishwarya Bhole <aishwarya.bhole@gmail.com>
2023-02-02 15:00:20 +0530

    second commit

--- examjava.java
+++ examjava.java
@@ -1,21 +1,70 @@
+import java.io.*;
+import java.util.Scanner;
+
 class examjava
+
+{
 public static void main (String args[])
 {
 
-    int n1 = 0; 
-    int n2 = 1; 
-    int n3;
-    int count = 20;
+    // int n1 = 0; 
+    // int n2 = 1; 
+    // int n3;
+    // int count = 20;
 
-    system.out.println(n1+" "+n2);
+    // system.out.println(n1+" "+n2);
 
-    for(i=2; i<count; ++i)
+    // for(i=2; i<count; ++i)
+    // {
+    //     n3 = n1+n2;
+    //     system.out.println(" "+n3);
+    //     n1 = n2;
+    //     n2 = n3;
+
+    // }
+
+    char num;
+String choice;
+
+do{
+
+    System.out.println("Press 1 for check prime numbe");
+    System.out.println("Press 0 for check prime numbe");
+    Scanner sc = new Scanner (System.in);
+    int s = sc.nextInt();
+
+    switch (choice)
+{
+    case 1: 
     {
-        n3 = n1+n2;
-        system.out.println(" "+n3);
-        n1 = n2;
-        n2 = n3;
-
+    int n, i , res;
+    boolean flag = true;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please enter number:");
+    n = scan.nextInt();
+    for(i = 2; i<n/2; i++)
+    {
+        res = n%i;
+        if(res ==0)
+        {
+            flag = false;
+            break;
+        }
     }
 
+    if(flag)
+    System.out.println("Number is prime number");
+    else
+    System.out.println("Number is not prime number");
+    break;
+}
+
+
+case 0: 
+System.out.println("exit");
+
+} while(choice != 0);
+    
+}
+}
 }
